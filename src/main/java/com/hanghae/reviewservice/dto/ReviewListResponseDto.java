faca8fcaeef8a701ac8c72d7a4f093e0ad016051
mjyoo <ryuminji0353@gmail.com>
2024-12-12 19:50:10 -0800

    [feat] averageScore 소수점 첫째자리까지만 출력되도록 설정

--- src/main/java/com/hanghae/reviewservice/dto/ReviewListResponseDto.java
+++ src/main/java/com/hanghae/reviewservice/dto/ReviewListResponseDto.java
@@ -2,20 +2,23 @@
 package com.hanghae.reviewservice.dto;
 
 import lombok.Getter;
+
+import java.text.DecimalFormat;
 import java.util.List;
 
 
 @Getter
 public class ReviewListResponseDto {
     private Long totalReviewCount;
-    private double averageScore; //리뷰 평균 점수 (1-5)
+    private String averageScore; //리뷰 평균 점수 (1-5)
     private Long cursor; //위치
     private List<ReviewResponseDto> reviewList;
 
-    public ReviewListResponseDto(List<ReviewResponseDto> reviewList, Long totalReviewCount, double averageScore, Long cursor) {
+    public ReviewListResponseDto(List<ReviewResponseDto> reviewList, Long totalReviewCount, float averageScore, Long cursor) {
         this.reviewList = reviewList;
         this.totalReviewCount = totalReviewCount;
-        this.averageScore = averageScore;
+        DecimalFormat df = new DecimalFormat("#.#");
+        this.averageScore = df.format(averageScore);
         this.cursor = cursor;
     }
 
